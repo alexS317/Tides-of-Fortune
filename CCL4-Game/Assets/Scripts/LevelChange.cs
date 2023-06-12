da191d9e938f9631695787f62dd29980c9be1ae0
jbluemchen <julia.blum0@gmail.com>
2023-06-12 13:58:16 +0200

    Level change functionality

---
+++ CCL4-Game/Assets/Scripts/LevelChange.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChange : MonoBehaviour
+{
+    [SerializeField] private string level;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("HI");
+            SceneManager.LoadSceneAsync(level);
+        }
+    }
+}
