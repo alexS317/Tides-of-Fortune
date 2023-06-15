7fa92faf836419ca90c30fd9d22b9f300f2082eb
Alexandra F <93889946+alexS317@users.noreply.github.com>
2023-06-15 20:04:51 +0200

    Adding ocean

---
+++ CCL4-Game/Assets/Scripts/Ocean.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ocean : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadSceneAsync("GameOver");
+        }
+        
+        Destroy(other.gameObject);
+    }
+}
