cb6f15b2d52044266bccf255a16a57ce7289e837
cc211001 <132071335+cc211001@users.noreply.github.com>
2023-06-19 12:42:24 +0200

    berry attack

--- CCL4-Game/Assets/Scripts/NPCs/Tree/Berry.cs
+++ CCL4-Game/Assets/Scripts/NPCs/Tree/Berry.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Berry : MonoBehaviour
 {
+
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if(timer > 2)
+        {
+
+            print("berry bad");
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        print("Damage");
     }
 }
